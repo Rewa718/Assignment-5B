30d15a4dc1ba9a2cdf4159703cd3d009497a365c
Rewa Patil <>
2021-05-11 21:10:50 +0000

    indentation

--- script.js
+++ script.js
@@ -12,13 +12,12 @@ function myArea () {
   document.getElementById('displayText').innerText = 'Area is'
   document.getElementById('displayans').innerText = ans
 } function myCircumference () {
-   const entradius = parseInt(document.getElementById('entradius').value)
+  const entradius = parseInt(document.getElementById('entradius').value)
 
   // calculate circumference of circle
   const ans = 2 * 3.14 * entradius
   document.getElementById('displayText').innerText = 'Circumference is'
   document.getElementById('displayans').innerText = ans
-
 } function myEquation () {
   document.getElementById('displayText').innerText = 'A = πr^2 '
   document.getElementById('displayans').innerText = 'C = 2πr'
